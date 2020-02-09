e64f735f9bc710fa42390c368cec54ad9add6a9b
David Kanenwisher <d.kanen@gmail.com>
2020-02-09 16:45:48 -0600

    Let the adventurers talk!

--- src/main/kotlin/com/dkanen/Adventurer.kt
+++ src/main/kotlin/com/dkanen/Adventurer.kt
@@ -5,5 +5,21 @@ class Adventurer(val name: String) {
         return (++location)
     }
 
+    fun talk(): String {
+        return "You talk to no one in particular."
+    }
+
+    fun listen(): String {
+        return "There is no sound to hear."
+    }
+
+    fun talk(listener: Adventurer): String {
+       return "Talking to yourself may be a sign of genius"
+    }
+
+    fun heard(): String {
+       return "How fair the beets at this establishment?"
+    }
+
     var location: Int = 0
 }
--- src/test/kotlin/com/dkanen/AdventurerTest.kt
+++ src/test/kotlin/com/dkanen/AdventurerTest.kt
@@ -4,28 +4,54 @@ import kotlin.test.Test
 import kotlin.test.*
 
 class AdventurerTest {
+
+    lateinit var susan: Adventurer
+
+    @BeforeTest
+    fun setUp() {
+        susan = Adventurer("Susan the Valiant")
+    }
+
     @Test
     fun `it has a name`() {
-        val susan = Adventurer("Susan the Valiant")
         assertEquals("Susan the Valiant", susan.name)
     }
 
     @Test
     fun `it has a position in 1 dimension`() {
-        val susan = Adventurer("Susan the Valiant")
         assertEquals(0, susan.location)
     }
 
     @Test
     fun `it can walk in one space`() {
-        val susan = Adventurer("Susan the Valiant")
         assertEquals(1, susan.walk())
     }
 
     @Test
     fun `it can walk two spaces`() {
-        val susan = Adventurer("Susan the Valiant")
         assertEquals(1, susan.walk())
         assertEquals(2, susan.walk())
+    }
+
+    @Test
+    fun `it can talk to no one in particular and get a response that's there nobody to talk to`() {
+        assertEquals("You talk to no one in particular.", susan.talk())
+    }
+
+    @Test
+    fun `it can listen to no one in particular`() {
+        assertEquals("There is no sound to hear.", susan.listen())
+    }
+
+    @Test
+    fun `it can listen to itself`() {
+        assertEquals("Talking to yourself may be a sign of genius", susan.talk(susan))
+    }
+
+    @Test
+    fun `it can talk to somebody else`() {
+        val betty = Adventurer("Betty the Bold")
+        susan.talk(betty)
+        assertEquals("How fair the beets at this establishment?", betty.heard() )
     }
 }
