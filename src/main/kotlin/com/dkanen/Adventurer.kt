a616958372d058ad2697d9df58b50b1249e2bd84
David Kanenwisher <d.kanen@gmail.com>
2020-02-15 21:21:58 -0600

    Switch Adventurer to Single Expression Functions.
    Because they are so delightfully simple!

--- src/main/kotlin/com/dkanen/Adventurer.kt
+++ src/main/kotlin/com/dkanen/Adventurer.kt
@@ -1,25 +1,17 @@
 package com.dkanen
 
 class Adventurer(val name: String) {
-    fun walk(): Int {
-        return (++location)
-    }
-
-    fun talk(): String {
-        return "You talk to no one in particular."
-    }
-
-    fun listen(): String {
-        return "There is no sound to hear."
-    }
-
-    fun talk(listener: Adventurer): String {
-       return "Talking to yourself may be a sign of genius"
-    }
-
-    fun heard(): String {
-       return "How fair the beets at this establishment?"
-    }
 
     var location: Int = 0
+
+    fun walk(): Int = (++location)
+
+    fun talk(): String = "You talk to no one in particular."
+
+    fun listen(): String = "There is no sound to hear."
+
+    fun talk(listener: Adventurer): String = "Talking to yourself may be a sign of genius"
+
+    fun heard(): String = "How fair the beets at this establishment?"
+
 }
