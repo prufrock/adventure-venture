03756b37b1711f8bbb8ade4db6ed607bbc9b6064
David Kanenwisher <d.kanen@gmail.com>
2020-03-31 20:04:32 -0500

    Create the GenericDispatcher
    The GenericDispatcher should be able to replace the current Dispatcher but support both Strings and Events with fewer methods. Sadly, it won't be able to support String and Events at the same time. There will need to be a different GenericDispatcher for each type in that scenario.
    
    The next step is to have the GenericDispatcher replace the Dispatcher in a refactoring switcheroo.

--- src/main/kotlin/com/dkanen/GenericDispatcher.kt
+++ src/main/kotlin/com/dkanen/GenericDispatcher.kt
@@ -2,4 +2,13 @@ package com.dkanen
 
 class GenericDispatcher<T> {
 
+    private val subscriberList: MutableList<(T) -> Unit> = mutableListOf()
+
+    fun subscribe(subscriberFunction: (T) -> Unit) {
+        subscriberList.add(subscriberFunction)
+    }
+
+    fun broadcast(event: T) {
+        subscriberList.map { subscriber -> subscriber(event)}
+    }
 }
--- src/test/kotlin/com/dkanen/GenericDispatcherTest.kt
+++ src/test/kotlin/com/dkanen/GenericDispatcherTest.kt
@@ -8,7 +8,48 @@ class GenericDispatcherTest {
 
     @Test
     fun `it can be instantiated`() {
-        val dispatcher = GenericDispatcher<String>()
+        val dispatcher = GenericDispatcher<Event>()
         assertTrue(true)
+    }
+
+    @Test
+    fun `more than one Subscriber can receive a broadcast`() {
+        val dispatcher = GenericDispatcher<Event>()
+
+        var firstValue: Event = EmittedEvent("")
+
+        dispatcher.subscribe { event -> firstValue = event }
+
+        var secondValue: Event = EmittedEvent("")
+
+        dispatcher.subscribe { event -> secondValue = event }
+
+        dispatcher.broadcast(EmittedEvent("droppedScone"))
+
+        assertEquals("droppedScone", firstValue.name)
+        assertEquals("droppedScone", secondValue.name)
+    }
+
+
+    @Test
+    fun `it can dispatch an empty event object`() {
+        val dispatcher = GenericDispatcher<Event>()
+
+        var passedValue: Event = EmptyEvent()
+
+        dispatcher.subscribe { event -> passedValue = event }
+
+        assertEquals("", passedValue.name)
+    }
+
+    @Test
+    fun `it can dispatch an emitted event object`() {
+        val dispatcher = GenericDispatcher<Event>()
+
+        var passedValue: Event = EmittedEvent("emittedSound")
+
+        dispatcher.subscribe { event -> passedValue = event }
+
+        assertEquals("emittedSound", passedValue.name)
     }
 }
